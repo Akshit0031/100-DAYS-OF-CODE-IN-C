70b416c00bbbf8b828c327ebe9d1f7c31b966154
Akshit0031 <akshitruwari23@gmail.com>
2026-09-08 22:02:46 +0530

    day 30

---
+++ Q60.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n, positive = 0, negative = 0, zero = 0;
+
+    scanf("%d", &n);
+
+    int a[n];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+
+        if (a[i] > 0)
+            positive++;
+        else if (a[i] < 0)
+            negative++;
+        else
+            zero++;
+    }
+
+    printf("Positive=%d, Negative=%d, Zero=%d",
+           positive, negative, zero);
+
+    return 0;
+}
