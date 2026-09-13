04aa1241c1457371a20a7b432f54e694593d574a
Akshit0031 <akshitruwari23@gmail.com>
2026-09-13 21:31:25 +0530

    day 35

---
+++ Q69.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+int n; scanf("%d",&n); int a[n];
+for(int i=0;i<n;i++) scanf("%d",&a[i]);
+int max=a[0], second=a[0];
+for(int i=1;i<n;i++){
+if(a[i]>max){second=max;max=a[i];}
+else if(a[i]>second && a[i]!=max) second=a[i];
+}
+printf("%d",second);
+}
