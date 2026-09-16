29159e6210b38f08eace6ea45c3af914883b2469
Akshit0031 <akshitruwari23@gmail.com>
2026-09-16 22:01:52 +0530

    day 38

---
+++ Q76.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int a[10][10],n,i,j, flag=1;
+    scanf("%d%d", &n,&n);
+    for(i=0; i<n; i++)
+    for(j=0; j<n; j++)
+    scanf("%d",&a[i][j]);
+    for(i=0; i<n; i++)
+    for(j=0; j<n; j++)
+    if(a[i][j]!=a[j][i])
+    flag=0;
+    if(flag)
+    printf("true");
+    else
+    printf("false");
+    return 0;
+}
