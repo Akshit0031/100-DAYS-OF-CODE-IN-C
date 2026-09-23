596b5998a00e9a34dad54d2e1c908965ea93a66b
Akshit0031 <akshitruwari23@gmail.com>
2026-09-23 22:23:44 +0530

    day 45

---
+++ Q89.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    char s[100], ch;
+    int i, count = 0;
+
+    fgets(s, sizeof(s), stdin);
+    scanf("%c", &ch);
+
+    for(i = 0; s[i] != '\0'; i++) {
+        if(s[i] == ch)
+            count++;
+    }
+
+    printf("%d", count);
+
+    return 0;
+}
---
+++ Q90.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    char s[100];
+    int i;
+
+    fgets(s, sizeof(s), stdin);
+
+    for(i = 0; s[i] != '\0'; i++) {
+        if(s[i] >= 'a' && s[i] <= 'z')
+            s[i] = s[i] - 32;
+        else if(s[i] >= 'A' && s[i] <= 'Z')
+            s[i] = s[i] + 32;
+    }
+
+    printf("%s", s);
+
+    return 0;
+}
